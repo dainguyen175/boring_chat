2ee1e2455a4f6cbb5bd72133f9ee8bbfca44e8f7
dainguyen175 <dainguyennhu175@gmail.com>
2020-03-08 00:02:19 +0700

    update

---
+++ lang/vi.js
@@ -0,0 +1,6 @@
+export const transValidation = {
+  email_incorrect : "Email phải có dạng example@gmail.com!!",
+  gender_incorrect : "Ủa, sao lại sai giới tính vậy T_T",
+  password_incorrect : "Mật khẩu phải chứ ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, chữ số và ký tự đặc biệt. ",
+  password_confirmation_incorrect : "Nhập lại mật khẩu chưa chính xác.",
+};
