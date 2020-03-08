34a737d3e939d7535ed33c5bcb0b3cadbcd18f40
dainguyen175 <dainguyennhu175@gmail.com>
2020-03-09 03:34:48 +0700

    [1.0.0] Send email confirm

--- lang/vi.js
+++ lang/vi.js
@@ -1,18 +1,33 @@
 export const transValidation = {
   email_incorrect : "Email phải có dạng example@gmail.com!!",
   gender_incorrect : "Ủa, sao lại sai giới tính vậy T_T",
-  password_incorrect : "Mật khẩu phải chứ ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường, chữ số và ký tự đặc biệt. ",
+  password_incorrect : "Mật khẩu phải chứ ít nhất 8 ký tự, bao gồm chữ hoa, chữ thường và chữ số. ",
   password_confirmation_incorrect : "Nhập lại mật khẩu chưa chính xác.",
 };
 
 export const transErrors = {
-  account_in_use : "Email này đã được sử dụng",
-  account_removed: "Tài khoản này đã bị gỡ khỏi hệ thống, nếu tin rằng điều này là hiểu nhầm, vui lòng liện hệ với hỗ trợ của chúng tôi ",
-  account_not_active: "Email này đã được đăng ký nhưng chưa active tài khoản, vui lòng kiểu tra email của bạn hoặc liên hệ với bộ phận hỗ trợ của chúng tôi. "
+  account_in_use : "Email này đã được sử dụng.",
+  account_removed: "Tài khoản này đã bị gỡ khỏi hệ thống, nếu tin rằng điều này là hiểu nhầm, vui lòng liện hệ với hỗ trợ của chúng tôi. ",
+  account_not_active: "Email này đã được đăng ký nhưng chưa active tài khoản, vui lòng kiểu tra email của bạn hoặc liên hệ với bộ phận hỗ trợ của chúng tôi. ",
+  token_undefined: "Token không tồn tại!",
 };
 
 export const transSuccess = {
   userCreated: (userEmail) =>{
     return `Tài khoản <strong>${userEmail}</strong> đã được tạo, vui lòng kiểm tra email của bạn để active tài khoản trước khi đăng nhập. `;
-  }
+  },
+  account_actived: "Kích hoạt tài khoản thành công, bạn đã có thể đăng nhập vào ứng dụng."
+};
+
+export const transMail = {
+  subject: "Boring Chat: Xác nhận kích hoạt tài khoản",
+  template: (linkVerify) =>{
+    return `
+      <h2>Bạn nhận được email này vì đã đăng ký tài khoản trên ứng dụng Boring Chat. </h2>
+      <h3>Vui lòng click vào link bên dưới để xác nhận kích hoạt tài khoản. </h3>
+      <h3><a href="${linkVerify}" target="blank">${linkVerify}</h3>
+      <h4>Nếu tin rằng email này là nhầm lẫn, hãy bỏ qua nó. Trân trọng. </h4>
+    `;
+  },
+  send_failed:"Có lỗi trong quá trình gửi email, vui lòng liên hệ với bộ phận hỗ trợ của chúng tôi."
 };
