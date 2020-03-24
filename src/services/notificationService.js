d028472aef3575d3d256662dfedc7d0915aa21ec
dainguyen175 <dainguyennhu175@gmail.com>
2020-03-24 23:49:48 +0700

    [1.0.0] load more notif.

--- src/services/notificationService.js
+++ src/services/notificationService.js
@@ -1,15 +1,17 @@
 import notificationModel  from "./../model/notificationModel";
 import userModel from "./../model/userModel";
+
+
+const LIMIT_NUMBER_TAKEN= 10;
 /**
  * get notification when refresh page
  * just 10 items one time
- * @param {string} currentUserId 
- * @param {string} limit 
+ * @param {string} currentUserId  
  */
-let getNotifications = (currentUserId, limit =10) => {
+let getNotifications = (currentUserId) => {
   return new Promise(async (resolve, reject) =>{
     try {
-      let notifications = await notificationModel.model.getByUserIdAndLimit( currentUserId, limit);
+      let notifications = await notificationModel.model.getByUserIdAndLimit( currentUserId, LIMIT_NUMBER_TAKEN);
       
       let getNotfiContents = notifications.map(async (notification) => {
         let sender = await userModel.findUserById(notification.senderId);
@@ -38,7 +40,29 @@ let countNotifUnread = (currentUserId) => {
   })
 };
 
+/**
+ * Read more notification, max 10 items one time
+ * @param {string} currentUserId 
+ * @param {number} skipNumberNotification 
+ */
+let readMore = (currentUserId, skipNumberNotification) => {
+  return new Promise(async (resolve, reject) =>{
+    try {
+      let newNotifications = await notificationModel.model.readMore(currentUserId, skipNumberNotification, LIMIT_NUMBER_TAKEN);
+      
+      let getNotfiContents = newNotifications.map(async (notification) => {
+        let sender = await userModel.findUserById(notification.senderId);
+        return notificationModel.contents.getContent(notification.type, notification.isRead, sender._id, sender.username, sender.avatar);
+      });
+      
+      resolve( await Promise.all(getNotfiContents));
+    } catch (error) {
+      reject (error)
+    }
+  });
+};
 module.exports= {
   getNotifications: getNotifications,
   countNotifUnread: countNotifUnread,
+  readMore: readMore,
 };
