dbfb59e4c411e2ff29a34f482df46db519108920
dainguyen175 <dainguyennhu175@gmail.com>
2020-04-02 23:39:57 +0700

    update

--- src/sockets/index.js
+++ src/sockets/index.js
@@ -1,5 +1,7 @@
 import addNewContact from "./contact/addNewContact";
-import removeRequestContactSent from "./contact/removeRequestContactSent"
+import removeRequestContactSent from "./contact/removeRequestContactSent";
+import removeRequestContactReceived from "./contact/removeRequestContactReceived";
+
 
 /**
  * 
@@ -8,6 +10,7 @@ import removeRequestContactSent from "./contact/removeRequestContactSent"
 let initSockets = (io) => {
   addNewContact(io);
   removeRequestContactSent(io);
+  removeRequestContactReceived(io);
 };
 
 module.exports = initSockets;
