67a667eddacc155427a65daeeb75a962efe138ab
dainguyen175 <dainguyennhu175@gmail.com>
2020-05-11 22:55:22 +0700

    [1.0.0] set up turn server

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,5 +1,42 @@
 import {notification, contact, message} from "./../services/index";
 import {bufferToBase64, lastItemOfArray, convertTimestampToHumanTime} from "./../helpers/clientHelper";
+import request from "request";
+
+let getICETurnServer = () =>{
+  return new Promise(async (resolve,reject)=>{
+    // Node Get ICE STUN and TURN list
+    let o = {
+      format: "urls"
+    };
+
+    let bodyString = JSON.stringify(o);
+    
+    let options = {
+      url: "https://global.xirsys.net/_turn/Boring-chat",
+      // host: "global.xirsys.net",
+      // path: "/_turn/Boring-chat",
+      method: "PUT",
+      headers: {
+        "Authorization": "Basic " + Buffer.from("dainguyen175:051460f0-9398-11ea-b1d1-0242ac150006").toString("base64"),
+        "Content-Type": "application/json",
+        "Content-Length": bodyString.length
+      }
+    };
+
+    // call a request to get ICE list of turn server
+    request(options, (error, response, body) => {
+      if (error){
+        console.log("Error when get ICE list: "+ error);
+        return reject(error);
+      }
+      
+      let bodyJson = JSON.parse(body);
+      resolve(bodyJson.v.iceServers);
+    })
+
+    
+  });
+};
 
 let getHome = async (req, res) => {
   // only 10 items one time
@@ -27,6 +64,9 @@ let getHome = async (req, res) => {
   //all conversation with conversation, max 30 items
   let allConversationWithMessages = getAllConversationItems.allConversationWithMessages;
 
+  // get ICE list from xirsys turn server 
+  let iceServersList = await getICETurnServer();
+
   return res.render("main/home/home", {
     errors: req.flash("errors"),
     success: req.flash("success"),
@@ -43,6 +83,7 @@ let getHome = async (req, res) => {
     bufferToBase64: bufferToBase64,
     lastItemOfArray: lastItemOfArray,
     convertTimestampToHumanTime: convertTimestampToHumanTime,
+    iceServersList: JSON.stringify(iceServersList),
   });
 };
 
